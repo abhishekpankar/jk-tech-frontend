29add345e0f8462658c9e4c2a200155e3a7183df
Abhihsek Pankar <abhishek.pankar11@gmail.com>
2025-02-05 08:38:18 +0530

    guards and auth service added

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private jwtHelper = new JwtHelperService();
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  login(credentials: { email: string; password: string }) {
+    return this.http.post<{ access_token: string }>(`/api/v1/auth/login`, credentials)
+      .subscribe(response => {
+        localStorage.setItem('token', response.access_token);
+        this.router.navigate(['/home']);
+      });
+  }
+
+  isAuthenticated(){
+    const token = localStorage.getItem('token');
+    return token ? !this.jwtHelper.isTokenExpired(token) : false;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/home']);
+  }
+
+  getRole() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedToken = this.jwtHelper.decodeToken(token);
+      return decodedToken.role;
+    }
+    return null;
+  }
+}
