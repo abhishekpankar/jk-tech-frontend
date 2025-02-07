9d0a9442d4bfb92700b4d5bc6be979545375240b
Abhihsek Pankar <abhishek.pankar11@gmail.com>
2025-02-07 10:14:01 +0530

    blog component added

---
+++ src/app/components/blog/blog.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BlogService } from 'src/app/services/blog.service';
+import { Blog } from 'src/app/types/blogs.type';
+
+@Component({
+  selector: 'app-blog',
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.css']
+})
+export class BlogComponent {
+  blog!: Blog;
+  blogId!: number;
+
+  constructor(private readonly route: ActivatedRoute ,private readonly blogService: BlogService) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.blogId = params['id']!;
+    })
+    this.fetchBlog();
+  }
+
+  fetchBlog() {
+    this.blogService.fetchBlogById(this.blogId).subscribe((data) => {
+      this.blog = data.result;
+    })
+  }
+}
--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -2,19 +2,24 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ApiResponse } from '../types/response.type';
 import { Blog } from '../types/blogs.type';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
-  constructor(private readonly http: HttpClient) { }
+  constructor(private readonly http: HttpClient, private jwtService: JwtHelperService) {}
 
   fetchBlogs(page: number) {
-    return this.http.get<ApiResponse<Blog[]>>(`/api/v1/blogs`, {
+    return this.http.get<ApiResponse<Pick<Blog, 'title' | 'about' | 'createdAt' | 'id'>[]>>(`/api/v1/blogs`, {
       params: {
         page,
       }
     });
+  }
+
+  fetchBlogById(id: number) {
+    return this.http.get<ApiResponse<Blog>>(`/api/v1/blogs/${id}`);
   }
 }
